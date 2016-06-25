10c7e225581e005accc359405f4fcfd455c4b868
Hanif Fakira <fakira.work@gmail.com>
2016-06-25 14:23:40 +0100

    get about page

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -5,8 +5,15 @@ feature 'static_pages' do
     scenario 'should display contents' do
       visit '/'
       expect(page).to have_content 'Home Page'
+      expect(page).to have_link 'about'
     end
   end
 
-
+  context 'get about page ' do
+    scenario 'should display contents' do
+      visit '/'
+      click_link 'about'
+      expect(page).to have_content 'This is about the site'
+    end
+  end
 end
