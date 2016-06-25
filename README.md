5483678dee4585ed8dd8ce352e07745dfbdfc589
Hanif Fakira <fakira.work@gmail.com>
2016-06-25 14:35:48 +0100

    added css page

---
+++ README.md
@@ -0,0 +1,5 @@
+static page rails web application
+
+A place to practise using
+- javascript, jquery, ajax
+- styling such as: css, bootstrap, foundation
--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -6,6 +6,7 @@ feature 'static_pages' do
       visit '/'
       expect(page).to have_content 'Home Page'
       expect(page).to have_link 'about'
+      expect(page).to have_link 'css'
     end
   end
 
@@ -15,6 +16,26 @@ feature 'static_pages' do
       click_link 'about'
       expect(page).to have_content 'This is about the site'
       expect(page).to have_link 'home'
+      expect(page).to have_link 'css'
     end
+  end
+
+  context 'get css testing page ' do
+    scenario 'should display contents' do
+      visit '/'
+      click_link 'css'
+      expect(page).to have_content 'This shows examples of css'
+      expect(page).to have_link 'home'
+      expect(page).to have_link 'about'
+    end
+
+    scenario 'should reach page from about page' do
+      visit '/'
+      click_link 'about'
+      click_link 'css'
+      expect(page).to have_content 'This shows examples of css'
+    end
+
+
   end
 end
